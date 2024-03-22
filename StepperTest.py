f8e75c022ae4eb5b5b913a7dbfcc12e343243eef
project user <sattrac@mrgeiger.com>
2024-03-22 16:24:15 -0400

    Live Fixes to the mechanisms with a test bed

--- StepperTest.py
+++ StepperTest.py
@@ -6,96 +6,33 @@ import RPi.GPIO as GPIO
 from motor import Motor
 
 
-# MOTOR 2
-PUL = 16  # Stepper Drive Pulses
-DIR = 20  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
-ENA = 21  # Controller Enable Bit (High to Enable / LOW to Disable).
-
-motor = Motor(ENA, PUL, DIR, 1.8, 1)  #Changed ratio to 1 for trial
-sleep(10)
-
-
-
 # MOTOR 1
-#PUL = 17  # Stepper Drive Pulses
-#DIR = 27  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
-#ENA = 22  # Controller Enable Bit (High to Enable / LOW to Disable).
+PUL1 = 16  # Stepper Drive Pulses
+DIR1 = 20  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
+ENA1 = 21  # Controller Enable Bit (High to Enable / LOW to Disable).
 
-
+# MOTOR 2
+PUL2 = 17  # Stepper Drive Pulses
+DIR2 = 27  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
+ENA2 = 22  # Controller Enable Bit (High to Enable / LOW to Disable).
 
 
 GPIO.setmode(GPIO.BCM)
 
+motor1 = Motor(ENA1, PUL1, DIR1, 6400, 1)  #Changed ratio to 1 for trial
+motor2 = Motor(ENA2, PUL2, DIR2, 6400, 1)
+motor1.turn_degrees(-10)
+motor2.turn_degrees(-10)
+motor1.turn_degrees(10)
+motor2.turn_degrees(10)
+motor1.turn_to_degree(9)
+motor2.turn_to_degree(9)
+motor1.turn_to_degree(18)
+motor2.turn_to_degree(18)
+motor1.turn_to_degree(-18)
+motor1.turn_to_degree(-18)
+sleep(1)
 
-GPIO.setup(PUL, GPIO.OUT)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(ENA, GPIO.OUT)
-
-print('PUL = GPIO 17 - RPi 3B-Pin #11')
-print('DIR = GPIO 27 - RPi 3B-Pin #13')
-print('ENA = GPIO 22 - RPi 3B-Pin #15')
-
-print('Initialization Completed')
-#
-# Could have usesd only one DURATION constant but chose two. This gives play options.
-durationFwd = 6400 # This is the duration of the motor spinning. used for forward direction
-durationBwd = 6400 # This is the duration of the motor spinning. used for reverse direction
-print('Duration Fwd set to ' + str(durationFwd))
-print('Duration Bwd set to ' + str(durationBwd))
-#
-delay = 0.0001 # This is actualy a delay between PUL pulses - effectively sets the mtor rotation speed.
-print('Speed set to ' + str(delay))
-#
-cycles = 1 # This is the number of cycles to be run once program is started.
-cyclecount = 0 # This is the iteration of cycles to be run once program is started.
-print('number of Cycles to Run set to ' + str(cycles))
-#
-#
-def forward():
-    GPIO.output(ENA, GPIO.LOW)
-    print('ENA set to HIGH - Controller Enabled')
-    #
-    sleep(.5) # pause due to a possible change direction
-    GPIO.output(DIR, GPIO.LOW)
-    print('DIR set to LOW - Moving Forward at ' + str(delay))
-    print('Controller PUL being driven.')
-    for x in range(durationFwd): 
-        GPIO.output(PUL, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(PUL, GPIO.LOW)
-        sleep(delay)
-    GPIO.output(ENA, GPIO.HIGH)
-    print('ENA set to LOW - Controller Disabled')
-    sleep(.5) # pause for possible change direction
-    return
-#
-#
-def reverse():
-    GPIO.output(ENA, GPIO.LOW)
-    print('ENA set to HIGH - Controller Enabled')
-    #
-    sleep(.5) # pause due to a possible change direction
-    GPIO.output(DIR, GPIO.HIGH)
-    print('DIR set to HIGH - Moving Backward at ' + str(delay))
-    print('Controller PUL being driven.')
-    #
-    for y in range(durationBwd):
-        GPIO.output(PUL, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(PUL, GPIO.LOW)
-        sleep(delay)
-    GPIO.output(ENA, GPIO.HIGH)
-    print('ENA set to LOW - Controller Disabled')
-    sleep(.5) # pause for possible change direction
-    return
-
-while cyclecount < cycles:
-    forward()
-    reverse()
-    cyclecount = (cyclecount + 1)
-    print('Number of cycles completed: ' + str(cyclecount))
-    print('Number of cycles remaining: ' + str(cycles - cyclecount))
-#
 GPIO.cleanup()
 print('Cycling Completed')
 #
--- motor.py
+++ motor.py
@@ -1,6 +1,6 @@
 from time import sleep
 import RPi.GPIO as GPIO
-
+import math
 
 # Need to
 
@@ -8,47 +8,54 @@ class Motor:
     current_position: float = 0
     FORWARD: int = GPIO.LOW
     REVERSE: int = GPIO.HIGH
-    current_direction = FORWARD
+    
     DELAY: float = 0.001
 
-    def __init__(self, enable_pin, pulse_pin, direction_pin, motor_step_degrees=0, gear_ratio=0):
+    def __init__(self, enable_pin, pulse_pin, direction_pin, steps_per_rotation=0, gear_ratio=0):
+        GPIO.setup(pulse_pin, GPIO.OUT)
+        GPIO.setup(direction_pin, GPIO.OUT)
+        GPIO.setup(enable_pin, GPIO.OUT)
         self.enable_pin = enable_pin
         self.pulse_pin = pulse_pin
         self.direction_pin = direction_pin
-        self.motor_step_degrees = motor_step_degrees
+        self.steps_per_rotation = steps_per_rotation
         self.gear_ratio = gear_ratio
-        self.degrees = 0
-        self.pulse_degrees = motor_step_degrees / gear_ratio
-        self.turn_degrees(10)
-
+        self.current_position = 0
+        self.pulses_per_degree = 360/steps_per_rotation * gear_ratio
+        self.current_direction = self.FORWARD
+        
+        
     def step(self):
         GPIO.output(self.pulse_pin, GPIO.HIGH)
         sleep(self.DELAY)
         if self.current_direction == Motor.FORWARD:
-            self.current_position += self.motor_step_degrees
+            self.current_position += self.pulses_per_degree
         else:
-            self.current_position -= self.motor_step_degrees
+            self.current_position -= self.pulses_per_degree
         GPIO.output(self.pulse_pin, GPIO.LOW)
         sleep(self.DELAY)
 
     def turn_degrees(self, degrees_to_turn):
 
-        # needs to update current position
-
-        if degrees_to_turn > 0:
+        print(self.current_position)
+        
+        if degrees_to_turn <= 0:
             direction = self.FORWARD
         else:
             direction = self.REVERSE
         self.enable_motor()
         self.set_motor_direction(direction)
-        for x in [x * self.motor_step_degrees for x in range(0, degrees_to_turn)]:
+        pulses_to_move = int(math.fabs(degrees_to_turn/self.pulses_per_degree)+ 1)
+        for x in range(pulses_to_move):
             self.step()
         self.disable_motor()
+        
+        print(self.current_position);
 
-    def turn_to_heading(self, target):
-        # This should use current_position and turn_degrees to go to a specific angle
-        # Make sure we don't exceed turning limits (-180 to 180?) on az motor - cable wrap!
-        pass
+    def turn_to_degree(self, target_angle):
+        degrees_to_travel = self.current_position - target_angle 
+        
+        self.turn_degrees(degrees_to_travel)
 
     def enable_motor(self):
         GPIO.output(self.enable_pin, GPIO.LOW)
@@ -62,8 +69,9 @@ class Motor:
 
     def set_zero(self):
         self.current_position = 0
-        pass
 
     def goto_zero(self):
-        # move the moter from the current position to zero
-        pass
+        self.turn_to_degree(0)
+        
+    
+    
