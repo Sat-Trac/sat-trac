2fff6fea7254a639992488a1519eba604da4f7b7
project user <sattrac@mrgeiger.com>
2024-03-28 16:44:02 -0400

    Work from Friday

--- APIClass.py
+++ APIClass.py
@@ -4,24 +4,33 @@ import requests
 
 class APIClass:
     def __init__(self):
-        link = str(
-            "https://api.n2yo.com/rest/v1/satellite/positions/25544/41.5291/-84.306728/223/300/&apiKey=84KF72-KBE76V-3TF"
-            + "4JS-585A")
+        length = 3600*2
+        link = f'https://api.n2yo.com/rest/v1/satellite/positions/25544/41.5291/-84.306728/223/{length}/&apiKey=84KF72-KBE76V-3TF4JS-585A'
         response = requests.get(link)
         if response:
             self.retString = "Azimuth & Elevation Info"
-            r_dict = response.json()
-            for i in range(0, 300):
+            self.r_dict = response.json()
+            self.azList = []
+            self.elList = []
+            for i in range(0, length):
                 self.retString = self.retString + "\n" + "Azimuth: " + str(
-                    r_dict["positions"][i]["azimuth"]) + "; Elevation: " + str(
-                    r_dict["positions"][i]["elevation"]) + ";"
+                    self.r_dict["positions"][i]["azimuth"]) + "; Elevation: " + str(
+                    self.r_dict["positions"][i]["elevation"]) + ";"
+                self.azList.append(self.r_dict["positions"][i]["azimuth"])
+                self.elList.append(self.r_dict["positions"][i]["elevation"])
         else:
             print("Error: Something went wrong, try again.")
             exit()
+        print(len(response.json()["positions"]))
 
     def __str__(self):
         return self.retString
+    
+    def getNextInfo(self):
+        return (self.azList.pop(0), self.elList.pop(0))
 
 
-api = APIClass()
-print(api.__str__())
+#api = APIClass()
+#print(api.getNextInfo())
+#print(api.getNextInfo())
+#print(api.__str__())
--- StepperTest.py
+++ StepperTest.py
@@ -4,6 +4,7 @@
 from time import sleep
 import RPi.GPIO as GPIO
 from motor import Motor
+from APIClass import APIClass
 
 
 # MOTOR 1
@@ -19,19 +20,21 @@ ENA2 = 22  # Controller Enable Bit (High to Enable / LOW to Disable).
 
 GPIO.setmode(GPIO.BCM)
 
-motor1 = Motor(ENA1, PUL1, DIR1, 6400, 1)  #Changed ratio to 1 for trial
-motor2 = Motor(ENA2, PUL2, DIR2, 6400, 1)
-motor1.turn_degrees(-10)
-motor2.turn_degrees(-10)
-motor1.turn_degrees(10)
-motor2.turn_degrees(10)
-motor1.turn_to_degree(9)
-motor2.turn_to_degree(9)
-motor1.turn_to_degree(18)
-motor2.turn_to_degree(18)
-motor1.turn_to_degree(-18)
-motor1.turn_to_degree(-18)
-sleep(1)
+elevation_motor = Motor(ENA1, PUL1, DIR1, 6400, 1)  #Changed ratio to 1 for trial
+azimuth_motor = Motor(ENA2, PUL2, DIR2, 6400, 1)
+
+info = APIClass();
+while(True):
+    elv = info.getNextInfo()[0];
+    azi = info.getNextInfo()[1];
+    
+    elevation_motor.turn_to_degree(elv);
+    sleep(0.5);
+    azimuth_motor.turn_to_degree(azi);
+    sleep(0.5);
+    
+
+
 
 GPIO.cleanup()
 print('Cycling Completed')
